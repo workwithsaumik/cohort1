f66a5b6795457f28112be266988d34b8087b493d
Nirmal Hathi <nirmalhathis@win.dowjones.net>
2023-08-22 13:27:21 +0530

    fizzbuzz program

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,7 @@
-// // the hello world program
-// console.log('Hello World');
-var msg = "hello world";
-console.log(msg);
+// // // the hello world program
+// // console.log('Hello World');
+// var msg = "hello world";
+// console.log(msg);
 
+// // the hello world program
+// document.write('Hello, World!');
